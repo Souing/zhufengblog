5832b0034ac2665e4a0797fb2493f36ac26e1d95
zhangrenyang-t510 <zhang_renyang@>
2015-11-01 16:44:18 +0800

    init page

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,10 @@ router.get('/reg', function(req, res, next) {
   res.render('user/reg', { title: '用户注册' });
 });
 
+router.get('/reg', function(req, res, next) {
+  res.render('user/reg', { title: '用户注册' });
+});
+
 //登陆
 router.get('/login', function(req, res, next) {
   res.render('user/login', { title: '登陆' });
